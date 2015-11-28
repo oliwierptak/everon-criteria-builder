4b9e5f0395d1273d565761b62cc29f2dc70b4860
Oliwier Ptak <oliwierptak@gmail.com>
2015-11-28 21:28:16 +0100

    cleanups

--- tests/unit/ContainerTest.php
+++ tests/unit/ContainerTest.php
@@ -10,11 +10,9 @@
 namespace Everon\Component\CriteriaBuilder\Tests\Unit;
 
 use Everon\Component\CriteriaBuilder\Builder;
-use Everon\Component\CriteriaBuilder\CriteriaBuilderFactoryWorkerInterface;
+use Everon\Component\CriteriaBuilder\Criteria\ContainerInterface as CriteriaContainerInterface;
 use Everon\Component\CriteriaBuilder\CriteriaInterface;
 use Everon\Component\Factory\Dependency\Container;
-use Everon\Component\Factory\Dependency\ContainerInterface;
-use Everon\Component\Factory\FactoryInterface;
 use Everon\Component\Factory\Tests\Unit\Doubles\FactoryStub;
 use Everon\Component\Utils\TestCase\MockeryTest;
 use Everon\Component\Utils\Text\StartsWith;
@@ -26,73 +24,50 @@ class ContainerTest extends MockeryTest
     use StartsWith;
 
     /**
-     * @var CriteriaBuilderFactoryWorkerInterface
+     * @var CriteriaContainerInterface
      */
-    protected $CriteriaBuilderFactoryWorker;
+    protected $CriteriaContainer;
 
-    /**
-     * @var FactoryInterface
-     */
-    protected $Factory;
 
     protected function setUp()
     {
         $Container = new Container();
+        $Factory = new FactoryStub($Container);
+        $CriteriaBuilderFactoryWorker = $Factory->getWorkerByName('CriteriaBuilder', 'Everon\Component\CriteriaBuilder');
 
-        /* @var ContainerInterface $Container */
-        $this->Factory = new FactoryStub($Container);
-
-        $this->CriteriaBuilderFactoryWorker = $this->Factory->getWorkerByName('CriteriaBuilder', 'Everon\Component\CriteriaBuilder');
+        $Criteria = Mockery::mock('Everon\Component\CriteriaBuilder\CriteriaInterface');
+        /* @var CriteriaInterface  $Criteria */
+        $this->CriteriaContainer = $CriteriaBuilderFactoryWorker->buildCriteriaContainer($Criteria, Builder::GLUE_AND);
     }
 
     public function test_Constructor()
     {
-        $Criteria = Mockery::mock('Everon\Component\CriteriaBuilder\CriteriaInterface');
-
-        /* @var CriteriaInterface  $Criteria */
-        $Container = $this->CriteriaBuilderFactoryWorker->buildCriteriaContainer($Criteria, Builder::GLUE_AND);
-
-        $this->assertInstanceOf('Everon\Component\CriteriaBuilder\Criteria\ContainerInterface', $Container);
+        $this->assertInstanceOf('Everon\Component\CriteriaBuilder\Criteria\ContainerInterface', $this->CriteriaContainer);
     }
 
     public function test_glue_by_and()
     {
-        $Criteria = Mockery::mock('Everon\Component\CriteriaBuilder\CriteriaInterface');
+        $this->CriteriaContainer->resetGlue();
+        $this->assertEquals(null, $this->CriteriaContainer->getGlue());
 
-        /* @var CriteriaInterface  $Criteria */
-        $Container = $this->CriteriaBuilderFactoryWorker->buildCriteriaContainer($Criteria, Builder::GLUE_AND);
-
-        $Container->resetGlue();
-        $this->assertEquals(null, $Container->getGlue());
-
-        $Container->glueByAnd();
-        $this->assertEquals(Builder::GLUE_AND, $Container->getGlue());
+        $this->CriteriaContainer->glueByAnd();
+        $this->assertEquals(Builder::GLUE_AND, $this->CriteriaContainer->getGlue());
     }
 
     public function test_glue_by_or()
     {
-        $Criteria = Mockery::mock('Everon\Component\CriteriaBuilder\CriteriaInterface');
+        $this->CriteriaContainer->resetGlue();
+        $this->assertEquals(null, $this->CriteriaContainer->getGlue());
 
-        /* @var CriteriaInterface  $Criteria */
-        $Container = $this->CriteriaBuilderFactoryWorker->buildCriteriaContainer($Criteria, Builder::GLUE_AND);
-
-        $Container->resetGlue();
-        $this->assertEquals(null, $Container->getGlue());
-
-        $Container->glueByOr();
-        $this->assertEquals(Builder::GLUE_OR, $Container->getGlue());
+        $this->CriteriaContainer->glueByOr();
+        $this->assertEquals(Builder::GLUE_OR, $this->CriteriaContainer->getGlue());
     }
 
     public function test_reset_glue()
     {
-        $Criteria = Mockery::mock('Everon\Component\CriteriaBuilder\CriteriaInterface');
+        $this->CriteriaContainer->resetGlue();
 
-        /* @var CriteriaInterface  $Criteria */
-        $Container = $this->CriteriaBuilderFactoryWorker->buildCriteriaContainer($Criteria, Builder::GLUE_AND);
-
-        $Container->resetGlue();
-
-        $this->assertEquals(null, $Container->getGlue());
+        $this->assertEquals(null, $this->CriteriaContainer->getGlue());
     }
 
 }
