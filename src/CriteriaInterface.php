9a51029655188fbda9e6d310578ddf2cc87609ee
Oliwier Ptak <oliwierptak@gmail.com>
2015-12-11 19:57:23 +0100

    cleanups

--- src/CriteriaInterface.php
+++ src/CriteriaInterface.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * This file is part of the Everon framework.
+ * This file is part of the Everon components.
  *
- * (c) Oliwier Ptak <EveronFramework@gmail.com>
+ * (c) Oliwier Ptak <everonphp@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
